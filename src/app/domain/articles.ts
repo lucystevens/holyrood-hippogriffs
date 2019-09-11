bd4daf4ad1644b664f36a211ef07687065b0c0a4
lukecmstevens <lcm.stevens96@gmail.com>
2019-09-11 11:26:09 +0100

    Add article about freshers fair

--- src/app/domain/articles.ts
+++ src/app/domain/articles.ts
@@ -1,5 +1,21 @@
 import { Article } from "./data-definitions";
 
 export const ARTICLES : Article[] = [
-   
+    {
+        id: 1,
+        title: "Welcome Edinburgh students!",
+        image: "welcomeweek.jpg",
+        content: 
+            `Whether you're a fresher in your first year of University or
+            an aging PhD student looking for something new to try, come visit our fan club
+            at the Edinburgh University Students' Association Activities Fair! <br><br>
+            Come find the stall in the upper hall today or tomorrow from 10am-4pm to sign up!
+            `,
+        author: "Luke Stevens",
+        date: new Date(2019, 8, 11),
+        tags: [
+            "edinburgh",
+            "freshers"
+        ]
+    }
 ];
